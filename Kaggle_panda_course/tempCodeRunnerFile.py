f9ce516436377f54836df1d50f3fcab391cb536b
BinayakBishnu <bishnu.binayak12@gmail.com>
2021-09-01 13:12:37 +0530

    completed pandas

--- Kaggle_panda_course/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-# print(data.iloc[:3,0])  # 1st column for first 3 rows
-# print(data.iloc[5:9,2])  # 3rd column for 6th to 9th rows (excludes 9 index(10th row))
-# print(data.iloc[[3,8],1])  # 4th and 9th row, second column
-# print(data.iloc[-9:,0])  # 1st column for last 9
